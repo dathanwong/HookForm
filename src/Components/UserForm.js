8972a51461c102b715013225f4fd399a38e5c148
dathanwong <dwong5@villanova.edu>
2020-07-10 16:44:13 -0700

    Finished more forms assignment

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -9,29 +9,120 @@ const UserForm = (props) => {
     const [password, setpassword] = useState("");
     const [confirmPassword, setconfirmPassword] = useState("");
 
+    const [firstNameError, setFirstNameError] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+    const handleFirstName = (e) =>{
+        setfirstName(e.target.value);
+        if(e.target.value.length < 2 && e.target.value.length > 0){
+            setFirstNameError("First name must be at least 2 letters");
+        }else{
+            setFirstNameError("");
+        }
+    }
+
+    const handleLastName = (e) =>{
+        setlastName(e.target.value);
+        if(e.target.value.length < 2 && e.target.value.length > 0){
+            setLastNameError("Last name must be at least 2 letteres");
+        }else{
+            setLastNameError("");
+        }
+    }
+
+    const handleEmail = (e) =>{
+        setemail(e.target.value);
+        if(e.target.value.length < 2 && e.target.value.length > 0){
+            setEmailError("Email must be at least 2 letteres");
+        }else{
+            setEmailError("");
+        }
+    }
+
+    const handlePassword = (e) =>{
+        setpassword(e.target.value);
+        if(e.target.value.length < 8 && e.target.value.length > 0){
+            setPasswordError("Password must be at least 8 characters");
+        }else{
+            setPasswordError("");
+        }
+    }
+
+    const handleConfirmPassword = (e) =>{
+        setconfirmPassword(e.target.value);
+        if(e.target.value !== password && e.target.value.length > 0){
+            setConfirmPasswordError("Passwords must match");
+        }else{
+            setConfirmPasswordError("");
+        }
+    }
+
     return (  
         <div className="container">
             <div className="row my-2 justify-content-center">
                 <div className="col-6">
                     <div className= {"row py-2 my-2 justify-content-center " + styles.inputRow}>
                         <label className="col-3" for="">First Name</label>
-                        <input type="text" value={firstName} onChange={ e => setfirstName(e.target.value)}/>
+                        <input name="firstName" type="text" value={firstName} onChange={ e => handleFirstName(e)}/>
                     </div>
+                    {
+                        firstNameError ? 
+                        <div className= "row py-2 my-2 justify-content-center text-danger">
+                            {firstNameError}
+                        </div>
+                        :
+                        ""
+                    }
                     <div className={"row py-2 my-2 justify-content-center " + styles.inputRow}>
                         <label className="col-3" for="">Last Name</label>
-                        <input type="text" value={lastName} onChange={ e => setlastName(e.target.value)}/>
+                        <input name="lastName" type="text" value={lastName} onChange={ e => handleLastName(e)}/>
                     </div>
+                    {
+                        lastNameError ? 
+                        <div className= "row py-2 my-2 justify-content-center text-danger">
+                            {lastNameError}
+                        </div>
+                        :
+                        ""
+                    }
                     <div className={"row py-2 my-2 justify-content-center " + styles.inputRow}>
                         <label className="col-3" for="">Email</label>
-                        <input type="email" value={email} onChange={ e => setemail(e.target.value)}/>
+                        <input name="email" type="email" value={email} onChange={ e => handleEmail(e)}/>
                     </div>
+                    {
+                        emailError ? 
+                        <div className= "row py-2 my-2 justify-content-center text-danger">
+                            {emailError}
+                        </div>
+                        :
+                        ""
+                    }
                     <div className={"row py-2 my-2 justify-content-center " + styles.inputRow}>
                         <label className="col-3" for="">Password</label>
-                        <input type="password" value={password} onChange={ e=> setpassword(e.target.value)}/>
+                        <input name="password" type="password" value={password} onChange={ e=> handlePassword(e)}/>
                     </div>
+                    {
+                        passwordError ? 
+                        <div className= "row py-2 my-2 justify-content-center text-danger">
+                            {passwordError}
+                        </div>
+                        :
+                        ""
+                    }
+                    {
+                        confirmPasswordError ? 
+                        <div className= "row py-2 my-2 justify-content-center text-danger">
+                            {confirmPasswordError}
+                        </div>
+                        :
+                        ""
+                    }
                     <div className={"row py-2 my-2 justify-content-center " + styles.inputRow}>
                         <label className="col-3" for="">Confirm Password</label>
-                        <input type="password" value={confirmPassword} onChange = { e => setconfirmPassword(e.target.value)}/>
+                        <input name="confirmPassword" type="password" value={confirmPassword} onChange = { e => handleConfirmPassword(e)}/>
                     </div>
                 </div>
             </div>
